de752b9dad1c3ff2e6b4ce66467e4935dd094b57
Doramon <abc@gmail.com>
2017-07-28 17:01:21 +0700

    Success 280760

--- PKRU Service/ViewController.swift
+++ PKRU Service/ViewController.swift
@@ -13,10 +13,39 @@ class ViewController: UIViewController {
     //  Implicit
     var myString = ""
     
+    
+    @IBOutlet weak var UserTextField: UITextField!
+    
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    
+    @IBOutlet weak var AlertLabel: UILabel!
+    
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        
+        let strUser = UserTextField.text
+        let strPassword = PasswordTextField.text
+        
+        if (strUser == "") || (strPassword == "") {
+            // Have Space
+            AlertLabel.text = "Please Fill All Every Blank"
+        } else {
+            // No Space
+            
+        }
+        
+    }   // LoginButton
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }   // ViewDidLoad
+    
+    func checkUserAndPass(strUser: String, strPassword: String) -> Void {
+        
     }
 
     override func didReceiveMemoryWarning() {
